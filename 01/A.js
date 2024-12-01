4f47e4b5bf0729962428d5ff40e0fa7282028c86
CodedSakura <me@codedsakura.dev>
2024-12-01 14:40:09 +0200

    Day 01 - rewrite to use utils

--- 01/A.js
+++ 01/A.js
@@ -1,14 +1,10 @@
-const { readFileSync } = require("fs");
+const utils = require("../utils");
 
-const data = readFileSync("input.txt", "utf-8");
-
-console.log(data
-	.trim()
-	.split("\n")
-	.map(v => v.split(/\s+/).map(w => Number(w)))
-	.reduce((acc, row) => row.map((_, i) => [ ...(acc[i] ?? []), row[i] ]), [])
-	.map(v => v.sort((a, b) => a - b))
-	.reduce((acc, row) => row.map((_, i) => [ ...(acc[i] ?? []), row[i] ]), [])
+utils.read("input.txt")
+	.numbers2d()
+	.transpose()
+	.map(v => v.sortAsc())
+	.transpose()
 	.map(([a, b]) => Math.abs(a - b))
-	.reduce((acc, val) => acc + val)
-);
+	.sum()
+	.print();
