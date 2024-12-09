07ef5f507e49c74ff8d93e9678b98b514545f47b
CodedSakura <me@codedsakura.dev>
2024-12-09 13:42:01 +0200

    Day 09

--- 09/B.js
+++ 09/B.js
@@ -9,21 +9,29 @@ utils.read("input.txt")
 			gap.size -= file.size,
 			file.moved = true,
 			list.splice(list.indexOf(file), 1, { id: -1, size: file.size }),
-			list.splice(list.indexOf(gap), 0, file),
-			list.filter(v => v.size !== 0)
+			list.splice(list.indexOf(gap), gap.size === 0 ? 1 : 0, file),
+			list
 		) : list
 	)), a))
 	// <debug note="print filesystem representation (multidigit, strip trailing empty space)">
-	.run(d => (d.reduce(({ acc, idx }, { size, id }) => ({ 
-			acc: acc + (id === -1 ? '....|' : id.toString(10).padStart(4,'0') + '|').repeat(size),
-			idx: idx + size,
-		}), { acc: "|", idx: 0 }).acc.replace(/(?:\.+\|)+$/, '').print(), d))
+	// .run(d => (d.reduce(({ acc, idx }, { size, id }) => ({ 
+	// 		acc: acc + (id === -1 ? '....|' : id.toString(10).padStart(4,'0') + '|').repeat(size),
+	// 		idx: idx + size,
+	// 	}), { acc: "|", idx: 0 }).acc.replace(/(?:\.+\|)+$/, '').print(), d))
 	// </debug>
 	// <debug note="print filesystem representation (single digit)">
 	// .run(d => (d.reduce(({ acc, idx }, { size, id }) => ({ 
 	// 		acc: acc + (id === -1 ? '.' : id.toString(10)).repeat(size),
 	// 		idx: idx + size,
 	// 	}), { acc: "", idx: 0 }).acc.print(), d))
+	// </debug>
+	// <debug note="print first gap">
+	// .run(v => (
+	// 	idx = v.findIndex(v => v.id === -1).print("idx"),
+	// 	v[idx].print("gap"),
+	// 	v[idx+1].print("next"),
+	// 	v
+	// ))
 	// </debug>
 	.reduce(({ acc, idx }, { size, id }) => (id === -1 ? ({ acc, idx: idx + size }) : { 
 		acc: acc + (size)*(idx*2+size-1)/2 * id,
