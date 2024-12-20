4ef09b2a38f7bc6d61555d9939f19d4672052d67
CodedSakura <me@codedsakura.dev>
2024-12-20 15:22:45 +0200

    Day 20

---
+++ 20/A.js
@@ -0,0 +1,35 @@
+const utils = require("../utils");
+const { Pos } = utils; 
+
+utils.read("input.txt")
+	.chars2d()
+	.run(map => ({
+		size: map.size2d(),
+		start: new Pos(map.findIndex2d(v => v === 'S')),
+		end: new Pos(map.findIndex2d(v => v === 'E')),
+		tiles: new Set(map.map((r, y) => r.map((v, x) => v !== '#' ? new Pos(x, y) : null))
+			.flat().filter(v => v)),
+	}))
+	.run(data => (
+		({ start, end, tiles } = data),
+		tiles.delete(start),
+		remaining = new Set(tiles),
+		paths = [{ pos: start, score: 0, path: [ start ] }]
+			.while(
+				v => v.every(({ pos }) => pos.pos !== end.pos),
+				v => v.sort((a, b) => a.score - b.score)
+					.flatMap(({ pos, score, path }, i) => i !== 0 ? { pos, score, path } : pos.neighbors4(false)
+						.filter(n => remaining.has(n.pos))
+						.map(n => (remaining.delete(n.pos), { pos: n, score: score + 1, path: [ ...path, n ] }))
+					)
+			),
+		paths.find(p => p.pos.pos === end).path
+	))
+	.run(path => (
+		path.flatMap((f, i) => path.slice(i+1).map(t => [f, t]))
+			.filter(([a, b]) => (d = a.sub(b), Math.abs(d.x) + Math.abs(d.y)) == 2)
+			.map(([a, b]) => path.indexOf(b) - path.indexOf(a) - 2)
+			.filter(v => v >= 100)
+			.length
+	))
+	.print();
